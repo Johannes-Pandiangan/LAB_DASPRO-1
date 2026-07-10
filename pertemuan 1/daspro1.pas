b7124d99fc496e28742fd9ea68cf5e12dd2bb32c
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2026-07-10 10:20:41 +0700

    Pertemuan 1: Pengenalan pemrograman dan Pascal

---
+++ pertemuan 1/daspro1.pas
@@ -0,0 +1,18 @@
+program daspro1;
+uses crt;
+const 
+  g= 9.8;
+var 
+  nama: string;
+  nim: string;
+begin
+    clrscr;
+    writeln(g:2:1);
+    writeln ('Aku anak sehat');
+    writeln ('aku sehat');
+    write('nama: '); readln (nama);
+    write('nim: '); readln (nim);
+    (*cara buat komentar*)
+    {komentar juga}
+    //ini juga sama
+end.
---
+++ pertemuan 1/data.pas
@@ -0,0 +1,11 @@
+program biodata;
+uses crt;
+var
+   nama : string;
+   umur : integer;
+   cita : string;
+begin
+    clrscr;
+    write ('masukkan namamu: '); read(nama);
+    write ('numurmu: '); readln(umur);
+end.
